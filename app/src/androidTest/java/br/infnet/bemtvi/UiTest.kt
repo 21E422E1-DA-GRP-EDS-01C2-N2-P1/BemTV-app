1bd01ac6ea33fce959d77299097a2bc047e8fcaf
ecrseer <gabriel.jcouto@al.infnet.edu.br>
2022-03-07 21:43:57 -0300

    lab207

--- app/src/androidTest/java/br/infnet/bemtvi/UiTest.kt
+++ app/src/androidTest/java/br/infnet/bemtvi/UiTest.kt
@@ -1,10 +1,14 @@
 package br.infnet.bemtvi
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import br.infnet.bemtvi.ui.MainActivity
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -12,4 +16,13 @@ import org.junit.runner.RunWith
 class UiTest {
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun findLoginButtonByText(){
+        //onView(ViewMatchers.withId(R.))
+        onView(ViewMatchers.withText("entrar"))
+            .check(ViewAssertions.matches(ViewMatchers.isClickable()))
+        val d=0
+
+    }
 }
