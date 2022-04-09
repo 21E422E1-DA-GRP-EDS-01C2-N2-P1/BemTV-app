7402f5075b3f9932bb42a1be8994bb8d39f2c943
ecrseer <juustino@live.com>
2022-04-08 22:05:16 -0300

    banners adds

--- app/src/main/java/br/infnet/bemtvi/ui/MainActivity.kt
+++ app/src/main/java/br/infnet/bemtvi/ui/MainActivity.kt
@@ -9,6 +9,10 @@ import br.infnet.bemtvi.R
 import br.infnet.bemtvi.databinding.ActivityLoginBinding
 import br.infnet.bemtvi.ui.login.LoginFragment
 import br.infnet.bemtvi.ui.main.LoggedinFragment
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +39,39 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+    }
+    private fun adsPubBind(){
+        MobileAds.initialize(this)
+        val adView = binding.adView
+        val adBuild = AdRequest.Builder().build()
+
+        adView.loadAd(adBuild)
+
+
+        adView.adListener = object: AdListener() {
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                println("onAdFailedToLoad::: $adError")
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        }
     }
 
     override fun onStart() {
